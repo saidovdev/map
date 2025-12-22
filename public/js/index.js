3bdf7aefae3b3533e15772dea04ec66846bc61a3
Muhammadali Saidov <saidovdev@gmail.com>
2025-12-22 08:25:39 +0500

    Initial commit

---
+++ public/js/index.js
@@ -0,0 +1,38 @@
+const socket = io();
+
+const map = L.map("map").setView([0, 0], 10);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "© OpenStreetMap",
+  maxZoom: 19
+}).addTo(map);
+
+let marker;
+
+document.getElementById("start").addEventListener("click", () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+
+        socket.emit("send-location", { latitude, longitude });
+
+        if (!marker) {
+          marker = L.marker([latitude, longitude]).addTo(map);
+        } else {
+          marker.setLatLng([latitude, longitude]);
+        }
+
+        map.setView([latitude, longitude], 16);
+      },
+      (error) => {
+        console.log("GPS error:", error.message);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      }
+    );
+  }
+});
---
+++ server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+ res.render("index")
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+  socket.on('send-location',(data)=>{
+    
+  })
+});
+
+server.listen(3000, () => {
+  console.log("Server running on 3000");
+});
