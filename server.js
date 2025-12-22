a8389ae4bccbffcc8dc89f9d64f4f2764f1c388b
Muhammadali Saidov <saidovdev@gmail.com>
2025-12-22 08:35:55 +0500

    server

--- server.js
+++ server.js
@@ -1,29 +1,24 @@
-import express from "express";
-import http from "http";
-import { Server } from "socket.io";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 app.set("view engine", "ejs");
-
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
- res.render("index")
+  res.render("index");
 });
 
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
-  socket.on('send-location',(data)=>{
-    
-  })
+  socket.on("send-location", (data) => {
+    // location handling
+  });
 });
 
 server.listen(3000, () => {
